f7b85666d5a9b54e9d398db348e06056dccd26bd
sterrestelsel <toscha.nv@gmail.com>
2021-07-20 01:21:10 +0700

    All tests for Digital Lab are passed

--- digitallab/excelsior_internship/digitallab.cpp
+++ digitallab/excelsior_internship/digitallab.cpp
@@ -9,17 +9,17 @@ using namespace std;
 ifstream input("input.txt");
 ofstream output("output.txt");
 
-int** allocate_matrix(int n, int m) {
-	int** matrix = new int* [n];
+char** allocate_matrix(int n, int m) {
+	char** matrix = new char* [n];
 	for (int i = 0; i < n; i++) {
-		matrix[i] = new int[m];
+		matrix[i] = new char[m];
 	}
 	return matrix;
 }
 
 // class of matrixes
 class Matrix {
-	int** matrix;
+	char** matrix;
 	int n;
 	int m;
 public:
@@ -40,7 +40,7 @@ public:
 		}
 	}
 
-	int** read_matrix() {
+	char** read_matrix() {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				input >> this->matrix[i][j];
@@ -49,7 +49,7 @@ public:
 		return this->matrix;
 	}
 
-	int** print_matrix() {
+	char** print_matrix() {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				output << this->matrix[i][j] << " ";
@@ -57,6 +57,14 @@ public:
 			output << "\n";
 		}
 		return this->matrix;
+	}
+
+	char getValue(int row, int column) {
+		return matrix[row][column];
+	}
+
+	void setValue(int row, int column, int value) {
+		matrix[row][column] = value;
 	}
 
 	int getN() {
@@ -75,6 +83,42 @@ public:
 	}
 };
 
+
+int check_pattern(Matrix* pattern, Matrix* matrix, int column, int row) {
+	int n1 = pattern->getN();
+	int m1 = pattern->getM();
+	int is_id = 0;
+	for (int i = 0; i < n1; i++) {
+		for (int j = 0; j < m1; j++) {
+			if (matrix->getValue(row + i, column + j) == pattern->getValue(i, j)) {
+				continue;
+			}
+			else {
+				is_id = 1;
+				break;
+			}
+		}
+		if (is_id) { break; }
+	}
+	return is_id;
+}
+
+Matrix* mark_pattern(Matrix* pattern, Matrix* matrix, int column, int row) {
+	int n1 = pattern->getN();
+	int m1 = pattern->getM();
+	for (int i = 0; i < n1; i++) {
+		for (int j = 0; j < m1; j++) {
+			if (matrix->getValue(row + i, column + j) == '1') {
+				matrix->setValue(row + i, column + j, '2');
+			}
+			else {
+				matrix->setValue(row + i, column + j, '*');
+			}
+		}
+	}
+	return matrix;
+}
+
 Matrix* search_pattern(Matrix* pattern, Matrix* matrix) {
 
 	int n1 = pattern->getN();
@@ -82,11 +126,15 @@ Matrix* search_pattern(Matrix* pattern, Matrix* matrix) {
 	int m1 = pattern->getM();
 	int m2 = matrix->getM();
 
-	for (int i = 0; i < n2 - n1; i++) {
-		for (int j = 0; j < m2 - m1; j++) {
-
+	for (int i = 0; i <= n2 - n1; i++) {
+		for (int j = 0; j <= m2 - m1; j++) {
+			if (!check_pattern(pattern, matrix, i, j)) {
+				mark_pattern(pattern, matrix, i, j);
+			}
 		}
 	}
+
+	return matrix;
 }
 
 int main() {
@@ -102,6 +150,10 @@ int main() {
 	input >> n >> m;
 	Matrix* searchm = new Matrix(n, m);
 	searchm->read_matrix();
+
+	Matrix* result = search_pattern(pattern, searchm);
+
+	result->print_matrix();
 
 	return 0;
 }
